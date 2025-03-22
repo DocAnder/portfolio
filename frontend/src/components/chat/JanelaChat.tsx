e9923be2b8edabe4c682061b0b4a705159222f3a
Anderson <andersonmartinseoliveira@gmail.com>
2025-03-22 13:56:14 -0300

    feat(frontend): cria a janela de chat para interação do user com o assistente

---
+++ frontend/src/components/chat/JanelaChat.tsx
@@ -0,0 +1,70 @@
+"use client"
+import useChat from "@/hooks/useChat";
+import { IconMessages, IconReload, IconSend } from "@tabler/icons-react";
+import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from "react";
+import BalaoMensagem from "./BalaoMensagem";
+import Image from "next/image";
+
+export default function JanelaChat(){
+
+    const { limparChat, mensagens, adicionarMensagem, pensando } = useChat();
+    const [texto, setTexto] = useState("");
+    const fimChatRef = useRef<HTMLDivElement>(null);
+
+    function envitarMensagem(){
+        adicionarMensagem(texto);
+        setTexto("");
+    }
+
+    useEffect(() => {
+        fimChatRef.current?.scrollIntoView({behavior: "smooth"});
+    },[mensagens]);
+
+    return (
+        <div className="flex flex-col bg-zinc-300 rounded-2xl text-black overflow-hidden">
+            <div className="flex justify-between items-center bg-white p-4">
+                <h2 className="text-xl font-bold">Olá Visitante!</h2>
+                <IconReload
+                 size={24}
+                 className="text-black cursor-pointer"
+                 onClick={limparChat} />
+            </div>
+            {mensagens.length === 0 ? (
+                <div className="flex flex-col justify-center items-center min-h-[400px] sm:min-h-[300px]">
+                    <IconMessages
+                    size={230}
+                    stroke={0.2}
+                    className="text-black/30"/>
+                    <span>Vamos conversar?</span>
+                </div>
+            ) : (
+                <div className="flex flex-col p-2 gap-2 min-h-[300px] sm:min-h-[200px] overflow-y-scroll sm:max-h-[400px] max-h-[300px]">
+                    {mensagens.map((mensaggem) => {
+                        return <BalaoMensagem key={mensaggem.id} mensagem={mensaggem} />
+                    })}
+                    {pensando && (
+                        <Image src={"/pensando.gif"} alt="Pensando" width={50} height={50} />
+                    )}
+                    <div ref={fimChatRef}/>
+                </div>
+            )}
+            <div className="h-px bg-zinc-400 mt-4"/>
+            <div className="flex items-center gap-2 p-1 m-4 rounded-full h-10 bg-white">
+                <input 
+                type="text"
+                value={texto}
+                className="flex-1 bg-transparent h-8 outline-none pl-3"
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    setTexto(e.target.value);
+                }}
+                onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
+                    if(e.key === "Enter") envitarMensagem()
+                }}
+            />
+            <button className="bg-red-500 flex justify-center items-center min-h-8 min-w-8 rounded-full" onClick={envitarMensagem}>
+                <IconSend className="text-white" size={18} />
+            </button>
+            </div>
+        </div>
+    )
+}
